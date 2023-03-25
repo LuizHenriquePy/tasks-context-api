d48cac2e1508f55cbcd8876c06d7be6d38819468
luiz henrique <luizhenriquemendes123123@gmail.com>
2023-03-24 21:06:52 -0300

    testa se é possivel adicoinar task

---
+++ tests/ToDoList.test.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import ToDoList from '../src/pages/ToDoList'
+import userEvent from '@testing-library/user-event'
+import { AppContextProvider } from '../src/context/AppContextProvider'
+
+
+describe('Page ToDoList', () => {
+  it('Testa se é possível adicionar uma nova tarefas', async () => {
+    const user = userEvent.setup()
+    render(<AppContextProvider><ToDoList /></AppContextProvider>)
+    const inputAddTask = screen.getByRole('textbox')
+    const buttonAddTask = screen.getByRole('button', { name: /add task/i })
+    const text = 'texto para testar a tarefa'
+
+    await user.type(inputAddTask, text)
+    await user.click(buttonAddTask)
+
+    const task = await screen.findByRole('heading', {
+      name: text
+    })
+
+    expect(task).toBeVisible()
+    expect(task).toBeInTheDocument()
+  })
+})
